f1c711500c61b1a4536d28c3a5df734f1322e093
Naufal Hardiansyah <naufalh39@gmail.com>
2023-05-18 22:16:30 +0700

    feat: recipe bookmark feature with supabase

--- src/lib/components/FoodCard.svelte
+++ src/lib/components/FoodCard.svelte
@@ -2,37 +2,104 @@
 	import Icon from 'svelte-awesome';
 	import heart from 'svelte-awesome/icons/heart';
 	import { page } from '$app/stores';
-	import { getContext } from 'svelte';
-	export let recipe: App.Recipe;
-	let heartStyle: string;
-	let loading: boolean = false;
+	import { getContext, onMount } from 'svelte';
 	import type { DrawerSettings } from '@skeletonlabs/skeleton';
-	import { drawerStore } from '@skeletonlabs/skeleton';
-	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
-	import { toastStore } from '@skeletonlabs/skeleton';
-	import type { ToastSettings } from '@skeletonlabs/skeleton';
+	import type { User } from '@supabase/supabase-js';
+	import { drawerStore, toastStore } from '@skeletonlabs/skeleton';
+	import { enhance, type SubmitFunction } from '$app/forms';
 	const { showModalAuth } = getContext('authentication');
 	const { removePlaceholder } = getContext('placeholder');
 
-	const toggleFav = () => {
-		if (!$page.data.session) {
+	export let recipe: App.Recipe;
+	// export let user: User;
+	let heartStyle: string;
+	let loading: boolean = false;
+
+	// export let data:any;
+	$: ({ supabase, session } = $page.data);
+	$: fillRed = recipe.bookmarked ? '!fill-red-500' : '';
+
+	onMount(async () => {		
+		try {
+			if (!session?.user) return;
+
+			const { count, error } = await supabase
+				.from('favourites')
+				.select('*', { count: 'exact', head: true })
+				.eq('recipe_id', recipe.id)
+				.eq('user_id', session?.user.id);
+
+			console.log('data', count);
+			if (error) throw error;
+
+			if (!count) recipe.bookmarked = false;
+			else {
+				console.log('Recipe ', recipe.title, ' is bookmarked.');
+				recipe.bookmarked = true;
+			}
+		} catch (error) {
+			toastStore.trigger({
+				message: error as string,
+				background: 'variant-filled-error'
+			});
+		} finally {
+			// Combine Spooncular likes with Supabase likes =)
+			const { count } = await supabase
+				.from('favourites')
+				.select('*', { count: 'exact', head: true })
+				.eq('recipe_id', recipe.id);
+
+			heartStyle = recipe.bookmarked ? '!fill-red-500' : '';
+			recipe.likes += count;
+		}
+	});
+
+	const toggleFav = async () => {
+		if (!session?.user) {
 			showModalAuth();
 			return;
 		}
 
-		// recipe.likes += recipe.isFav ? -1 : 1;
-		recipe.bookmarked = !recipe.bookmarked;
-		const fillRed = recipe.bookmarked ? '!fill-red-500' : '';
-		heartStyle = `${fillRed} animate-bounce`;
-		setTimeout(() => {
-			heartStyle = fillRed;
-		}, 500);
+		try {
+			heartStyle = `${fillRed} animate-bounce`;
+			recipe.likes += recipe.bookmarked ? -1 : 1;
+			recipe.bookmarked = !recipe.bookmarked;
+			if (recipe.bookmarked) {
+				console.log('Liking..');
+				const { data, error } = await supabase
+					.from('favourites')
+					.insert({ recipe_id: recipe.id, user_id: session?.user.id });
+				if (error) {
+					throw error;
+				}
+			} else {
+				console.log('Unliking..');
+				const { data, error } = await supabase
+					.from('favourites')
+					.delete()
+					.eq('recipe_id', recipe.id)
+					.eq('user_id', session?.user.id);
+				if (error) {
+					throw error;
+				}
+			}
+			console.log('Done!');
+		} catch (error) {
+			console.log(error);
+			recipe.likes += recipe.bookmarked ? -1 : 1;
+			recipe.bookmarked = !recipe.bookmarked;
+			toastStore.trigger({
+				message: error as string,
+				background: 'variant-filled-error'
+			});
+		} finally {
+			heartStyle = recipe.bookmarked ? fillRed : '';
+		}
 	};
 
 	const handleRecipeDetail: SubmitFunction = () => {
 		loading = true;
 		return async ({ result }) => {
-			let t: ToastSettings;
 			if (result.type === 'success') {
 				// Strip HTML tags from summary
 				let recipeDetail = result.data?.detail;
@@ -51,16 +118,15 @@
 						recipeEquipments: result.data?.equipments,
 						recipeInstructions: result.data?.instructions
 					}
-				};				
+				};
 				drawerStore.open(drawerSettings);
 
-				console.log('recipeInstructions', result.data?.instructions)
+				console.log('recipeInstructions', result.data?.instructions);
 			} else if (result.type === 'error') {
-				t = {
+				toastStore.trigger({
 					message: `Internal Server Error. Please try again later.`,
 					background: 'variant-filled-error'
-				};
-				toastStore.trigger(t);
+				});
 			}
 			loading = false;
 		};
